79b98947d80f3d782f704f0aad8ea49ad7ecb1fa
esteban.marcelloni@gmail.com <esteban.marcelloni@gmail.com>
2022-01-28 17:56:45 -0300

    sentence 2 vec

--- kaggle/08 - Competitions/02 - Jigsaw toxic severity rating/06_tf_idf.py
+++ kaggle/08 - Competitions/02 - Jigsaw toxic severity rating/06_tf_idf.py
@@ -1,14 +1,9 @@
-
 import pandas as pd
 from sklearn import model_selection
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.linear_model import PassiveAggressiveClassifier
-from resources.clean_data import clean_data
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
+from time import time
 
 # Load training data
 training_df = pd.read_csv('./data/ruddit_comments_score.csv')
@@ -18,21 +13,9 @@ training_df = pd.read_csv('./data/ruddit_comments_score.csv')
 # training_df = training_df.iloc[clean_df.index]
 # training_df['body'] = clean_df.copy()
 
-# Make our bag of word and then split. We can do this just because is Bag of Words
+# Make our TF-IDF
 y = training_df.pop('score')
 X = training_df['body'].copy()
-
-# X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, 
-#     test_size=0.33, random_state=0)
-# print('Train Shape: ', X_train.shape, 'Train label shape:', y_train.shape)
-# print('Test Shape: ', X_test.shape, 'Test label shape:', y_test.shape)
-
-# tfidf_vectorizer = TfidfVectorizer(strip_accents='unicode', 
-#     lowercase=True, 
-#     ngram_range=(1,3),
-#     stop_words='english')
-# tfidf_train = tfidf_vectorizer.fit_transform(X_train)
-# tfidf_test = tfidf_vectorizer.transform(X_test)
 
 tfidf_vectorizer = TfidfVectorizer()
 X_tfidf = tfidf_vectorizer.fit_transform(X)
@@ -71,7 +54,11 @@ def model_predict(less_toxic, more_toxic, model):
 validation_data = pd.read_csv('./data/validation_data.csv')
 less_toxic_count = tfidf_vectorizer.transform(validation_data.less_toxic)
 more_toxic_count = tfidf_vectorizer.transform(validation_data.more_toxic)
+print(less_toxic_count.shape)
+print(more_toxic_count.shape)
 
+t = time()
 # Test our model in validation dataset
 mean_score = model_predict(less_toxic_count[:], more_toxic_count[:], my_model)
-print(mean_score)
+print('Mean Score: ', mean_score)
+print('Time to validate: {} mins'.format(round((time() - t) / 60, 2)))
